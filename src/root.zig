43355e9f22bf61efa77f33ad05542b88bce1b205
Anze <peternelanze@gmail.com>
2026-03-02 08:17:13 +0100

    chore: add initial tests for different files

--- src/root.zig
+++ src/root.zig
@@ -5,7 +5,12 @@ test {
     _ = @import("./cli/handlers.zig");
     _ = @import("./cli/commands/config.zig");
     _ = @import("./cli/commands/runner.zig");
+    _ = @import("./cli/commands/report.zig");
+    _ = @import("./cli/commands/watch.zig");
+    _ = @import("./cli/commands/stats.zig");
     _ = @import("./conf/file.zig");
     _ = @import("./fs/directory_test.zig");
     _ = @import("./fs/watcher.zig");
+    _ = @import("./jobs/entry.zig");
+    _ = @import("./jobs/process.zig");
 }
