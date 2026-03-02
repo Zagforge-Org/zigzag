b999aaa55ca1b561a278ba841d5e1cd1bfd0b0bc
Anze <peternelanze@gmail.com>
2026-03-02 08:16:10 +0100

    chore: add additional binary support with line count

--- src/jobs/job.zig
+++ src/jobs/job.zig
@@ -4,6 +4,7 @@ const FileContext = @import("../cli/context.zig").FileContext;
 const CacheImpl = @import("../cache/impl.zig").CacheImpl;
 const ProcessStats = @import("../cli/commands/stats.zig").ProcessStats;
 const JobEntry = @import("entry.zig").JobEntry;
+const BinaryEntry = @import("entry.zig").BinaryEntry;
 
 pub const Job = struct {
     path: []const u8,
@@ -11,6 +12,7 @@ pub const Job = struct {
     cache: ?*CacheImpl,
     stats: *ProcessStats,
     file_entries: *std.StringHashMap(JobEntry),
+    binary_entries: *std.StringHashMap(BinaryEntry),
     entries_mutex: *std.Thread.Mutex,
     allocator: std.mem.Allocator,
 
