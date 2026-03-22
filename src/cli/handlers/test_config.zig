ca96ca111b86c7ada74cc9eb53e478f79381926e
Anze <peternelanze@gmail.com>
2026-03-22 10:21:35 +0100

    chore: refactor (remove old deps)

--- src/cli/handlers/test_config.zig
+++
@@ -1,6 +0,0 @@
-const std = @import("std");
-const Config = @import("../commands/config/config.zig").Config;
-
-pub fn makeTestConfig(allocator: std.mem.Allocator) Config {
-    return Config.default(allocator);
-}
