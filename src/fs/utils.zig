d7b1bd219752ae2c148f69f00df4d937952c84ff
Anze <peternelanze@gmail.com>
2026-07-19 21:56:10 +0200

    clean up: remove dead code

--- src/fs/utils.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-
-/// Check if path exists
-pub fn exists(io: std.Io, path: []const u8) !bool {
-    std.Io.Dir.cwd().access(io, path, .{}) catch return false;
-    return true;
-}
